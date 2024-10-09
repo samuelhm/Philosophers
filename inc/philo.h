86d89639554d149c24157d9a5a535e1adfec7bc0
Samuel Hurtado <lnx.wifi@gmail.com>
2024-10-09 15:58:40 +0200

    ¨include¨

--- inc/philo.h
+++ inc/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 13:47:41 by shurtado          #+#    #+#             */
-/*   Updated: 2024/10/08 13:48:30 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/10/08 20:49:00 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 # define PHILO_H
 
 # include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <semaphore.h>
 
 #endif
